a93728209cd3b351b2078471dc52e7f9dcfeb5c6
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2025-03-14 15:44:21 +0500

    feat: load commands and execute at cli

---
+++ application/presentation/cli/__main__.py
@@ -0,0 +1,36 @@
+import argparse
+
+from dependency_injector.wiring import Provide, inject
+
+from application.presentation.cli import __name__ as module_name
+from application.presentation.cli import __version__ as cli_version
+from application.presentation.cli import containers, loader
+from application.presentation.cli.command import Command
+
+
+@inject
+def main(
+    commands: list[type[Command]] = Provide[containers.Container.commands],
+) -> None:
+    parser = argparse.ArgumentParser(prog=module_name)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{module_name} v{cli_version}",
+    )
+
+    subparsers = parser.add_subparsers(required=True)
+    loader.load_commands(subparsers, commands)
+
+    args = parser.parse_args()
+    loader.execute_command(args)
+
+
+if __name__ == "__main__":
+    container = containers.Container()
+    container.config.from_json("cli_config.json")
+    container.init_resources()
+    container.wire([__name__])
+
+    main()

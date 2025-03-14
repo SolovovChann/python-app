b71d7f98c1771e9c9069ea38a6f7da6555f64420
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2025-03-14 13:14:17 +0500

    feat: load_commands function

--- application/presentation/cli/loader.py
+++ application/presentation/cli/loader.py
@@ -1,5 +1,7 @@
 import argparse
-from typing import Any, Protocol
+from typing import Any, Iterable, Protocol
+
+from application.presentation.cli.command import Command
 
 
 class _SubParser(Protocol):
@@ -10,3 +12,24 @@ class _SubParser(Protocol):
         deprecated: bool = False,
         **kwargs: Any,
     ) -> argparse.ArgumentParser: ...
+
+
+def load_commands(
+    subparsers: _SubParser,
+    commands: Iterable[type[Command]],
+    *,
+    payload_key: str = "payload",
+    **command_init_kwargs: Any,
+) -> None:
+    for command in commands:
+        instance = command(**command_init_kwargs)
+        subparser = subparsers.add_parser(
+            name=instance.get_name(),
+            help=instance.get_help(),
+            description=instance.get_description(),
+        )
+
+        instance.add_arguments(subparser)
+
+        defaults_kwargs = {payload_key: instance.handle}
+        subparser.set_defaults(**defaults_kwargs)

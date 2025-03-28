b645147a0a4585b3d91137c964f8b3f1ab905fea
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2025-03-28 19:49:15 +0500

    refactor: remove dependency injector logic from exec module + read json config directly + default_logging_config constant ~ load_container -> load_config

--- application/presentation/cli/exec.py
+++ application/presentation/cli/exec.py
@@ -1,22 +1,23 @@
 import argparse
+import json
+import logging.config
 import os
 import sys
-
-from dependency_injector.wiring import Provide, inject
+from typing import Any
 
 from application.presentation.cli import __name__ as module_name
 from application.presentation.cli import __version__ as cli_version
-from application.presentation.cli import containers, loader
-from application.presentation.cli.command import Command
+from application.presentation.cli import loader
+from application.shared.factory import import_classes
 
 
-@inject
-def main(
-    commands: list[type[Command]] = Provide[containers.Container.commands],
-) -> None:
+def main() -> None:
+    command_names, logging_config = _load_config()
+    logging.config.dictConfig(logging_config)
+
     parser = _create_parser()
-
     subparsers = parser.add_subparsers(required=True)
+    commands = import_classes(command_names)
     loader.load_commands(subparsers, commands)
 
     args = parser.parse_args()
@@ -35,19 +36,28 @@ def _create_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def _load_container() -> None:
-    config_file = os.environ.get("CLI_CFG_FILE", "cli.cfg")
+DEFAULT_LOGGING_CONFIG: dict[str, Any] = {
+    "version": 1,
+    "formatters": {},
+    "handlers": {},
+    "loggers": {},
+}
 
-    container = containers.Container()
-    container.config.from_json(config_file, required=True)
-    container.init_resources()
-    container.wire([__name__])
 
-    if not list(container.commands()):
-        sys.stderr.write("No commands for CLI specified.\nCheck config file")
-        sys.exit(1)
+def _load_config() -> tuple[list[str], dict[str, Any]]:
+    with open(os.environ.get("CLI_CFG_FILE", "cli.cfg")) as config_file:
+        config: dict[str, Any] = json.load(config_file)
+        commands = config.get("commands", [])
+        logging_config = config.get("logging", DEFAULT_LOGGING_CONFIG)
+
+        if not commands:
+            error = "No commands for CLI specified.\nCheck config file\n"
+
+            sys.stderr.write(error)
+            sys.exit(1)
+
+        return commands, logging_config
 
 
 if __name__ == "__main__":
-    _load_container()
     main()
